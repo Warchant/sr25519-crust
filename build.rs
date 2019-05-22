856594c781d42341d39b1f82c194860e85cad530
Restyled.io <commits@restyled.io>
2019-05-22 15:32:21 +0000

    Restyled by rustfmt

--- build.rs
+++ build.rs
@@ -4,5 +4,7 @@ extern crate cbindgen;
 use cbindgen::generate;
 
 fn main() {
-    generate(Path::new(".")).unwrap().write_to_file(Path::new("./include/sr25519/sr25519.h"));
+    generate(Path::new("."))
+        .unwrap()
+        .write_to_file(Path::new("./include/sr25519/sr25519.h"));
 }
