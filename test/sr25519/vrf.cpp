eedeee5ac896c960e7d5df6922afa9a5a617fd7a
Harrm <abrehchs@gmail.com>
2020-12-11 18:55:13 +0300

    Refactor test

--- test/sr25519/vrf.cpp
+++ test/sr25519/vrf.cpp
@@ -74,47 +74,46 @@ TEST(VrfTest, SignAndCheck) {
   EXPECT_TRUE(res1.is_less);
 }
 
-TEST(VrfTest, SignAndVerifyTranscript) {
-  auto keypair =
-      "915bb406968655c3412df5773c3de3dee9f6da84668b5de8d2f34d0304d20b0bac5ea3a293dfd93859ee64a5b825937753864c19be857f045758dcae10259ba1049b21bb9cb88471b9dadb50b925135cfb291a463043635b58599a2d01b1fd18"_unhex;
-  std::array<uint8_t, SR25519_VRF_OUTPUT_SIZE + SR25519_VRF_PROOF_SIZE>
-      out_and_proof {};
-
-  auto message = "Hello, world!"_v;
-  auto limit = std::vector<uint8_t>(16, 0xAA);
-
+template <size_t MsgBufSize>
+Strobe128 makeTranscript(std::string_view message) {
   primitives::Transcript t;
   t.initialize({'B', 'A', 'B', 'E'});
-  char msg_array[14];
-  std::memcpy(msg_array, message.data(), message.size());
-  t.append_message({'m', 'e', 's', 's', 'a', 'g', 'e'}, msg_array);
+  char msg_buf[MsgBufSize];
+  std::fill_n(msg_buf, MsgBufSize, 0);
+  std::memcpy(msg_buf, message.data(), message.size());
+  t.append_message({'m', 'e', 's', 's', 'a', 'g', 'e'}, msg_buf);
 
   Strobe128 strobe;
   std::memcpy(strobe.state, t.data().data(), t.data().size());
   strobe.pos = t.state().current_position;
   strobe.cur_flags = t.state().current_state;
   strobe.pos_begin = t.state().begin_position;
+  return strobe;
+}
+
+TEST(VrfTest, SignAndVerifyTranscript) {
+  auto keypair =
+      "915bb406968655c3412df5773c3de3dee9f6da84668b5de8d2f34d0304d20b0bac5ea3a293dfd93859ee64a5b825937753864c19be857f045758dcae10259ba1049b21bb9cb88471b9dadb50b925135cfb291a463043635b58599a2d01b1fd18"_unhex;
+  std::array<uint8_t, SR25519_VRF_OUTPUT_SIZE + SR25519_VRF_PROOF_SIZE>
+      out_and_proof {};
+
+  auto message = "Hello, world!"s;
+  auto limit = std::vector<uint8_t>(16, 0xAA);
+
+  auto strobe1 = makeTranscript<14>(message);
 
   auto res1 =
       sr25519_vrf_sign_transcript(out_and_proof.data(), keypair.data(),
-                                  &strobe, limit.data());
+                                  &strobe1, limit.data());
   ASSERT_EQ(res1.result, SR25519_SIGNATURE_RESULT_OK);
-  EXPECT_TRUE(res1.is_less);
+  bool less = res1.is_less;
 
-  primitives::Transcript t1;
-  t1.initialize({'B', 'A', 'B', 'E'});
-  char msg_array1[14];
-  std::memcpy(msg_array1, message.data(), message.size());
-  t1.append_message({'m', 'e', 's', 's', 'a', 'g', 'e'}, msg_array1);
-  std::memcpy(strobe.state, t.data().data(), t.data().size());
-  strobe.pos = t.state().current_position;
-  strobe.cur_flags = t.state().current_state;
-  strobe.pos_begin = t.state().begin_position;
+  auto strobe2 = makeTranscript<14>(message);
 
   auto res2 = sr25519_vrf_verify_transcript(
-      keypair.data() + 64, &strobe, out_and_proof.data(),
+      keypair.data() + 64, &strobe2, out_and_proof.data(),
       out_and_proof.data() + SR25519_VRF_OUTPUT_SIZE, limit.data());
   ASSERT_EQ(res2.result, SR25519_SIGNATURE_RESULT_OK);
-  ASSERT_TRUE(res2.is_less);
+  ASSERT_EQ(less, res2.is_less);
 
 }
